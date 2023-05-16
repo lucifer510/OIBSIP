3ea2372900ed3c834b66e75d155fec0529cec66d
Mihir Prajapati <prajapatimihir799@gmail.com>
2023-05-16 12:32:43 +0530

    All Task Completed

---
+++ Level 1/Task 3/index.js
@@ -0,0 +1,23 @@
+var inputs = document.querySelectorAll(".panel input");
+var c = document.querySelector('#celsius');
+var f = document.querySelector('#fahrenheit');
+var k = document.querySelector('#kelvin');
+
+inputs.forEach(input =>{
+    input.addEventListener("input", e => {
+        const unit = e.target.id;
+        const v = parseInt(e.target.value);
+        if(unit === "celsius"){
+            f.value = parseFloat((v*1.8)+32).toFixed(4)*1;
+            k.value = parseFloat(v+273.15).toFixed(4)*1;
+        }
+        else if(unit === "fahrenheit"){
+            c.value = parseFloat((v-32)*5/9).toFixed(4)*1;
+            k.value = parseFloat(((v-32)*5/9)+273.15).toFixed(4)*1;
+        }
+        else if(unit === "kelvin"){
+            c.value = parseFloat(v-273.15).toFixed(4)*1;
+            f.value = parseFloat(((v-273.15)*9/5)+32).toFixed(4)*1;
+        }
+    });
+});
